399c8043ce5fb9eb0e07eec5d7859bc522f69b75
Aarone sasha <aarongarcia058@gmail.com>
2023-04-18 08:52:52 +0200

    Introducimos los métodos restantes en la clase

--- com.entornos.AaronGarciaSanchez.arrays/src/com/entornos/AaronGarciaSanchez/arrays/Arrays.java
+++ com.entornos.AaronGarciaSanchez.arrays/src/com/entornos/AaronGarciaSanchez/arrays/Arrays.java
@@ -5,13 +5,9 @@ package com.entornos.AaronGarciaSanchez.arrays;
  * de números enteros
  * 
  * @param array
- * 
  * @version 1.0
- * 
  * @author Aaron
- * 
  * @return
- * 
  **/
 public abstract class Arrays {
 
@@ -23,4 +19,65 @@ public abstract class Arrays {
 		return (double) sumatorio / array.length;
 	}
 
+	/**
+	 * Método para calcular la mediana de un array de números enteros Recibe un
+	 * array de números enteros.
+	 * 
+	 * @param array
+	 * @version 1.0
+	 * @author Aaron
+	 * @return devuelve la mediana con decimales.
+	 **/
+	public static double mediana(int[] array) {
+		Arrays.sort(array);
+		int middle = array.length / 2;
+		if (array.length % 2 == 0) {
+			return (double) (array[middle - 1] + array[middle]) / 2;
+		} else {
+			return (double) array[middle];
+		}
+	}
+
+	protected static void sort(int[] array) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Método para calcular el máximo valor de un array de números enteros Recibe un
+	 * array de números enteros
+	 * 
+	 * @param array
+	 * @version 1.0
+	 * @author Aaron
+	 * @return devuelve el valor máximo
+	 **/
+	public static int maximo(int[] array) {
+		int maximo = Integer.MIN_VALUE;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > maximo) {
+				maximo = array[i];
+			}
+		}
+		return maximo;
+	}
+
+	/**
+	 * Método para calcular el mínimo valor de un array de números enteros Recibe un
+	 * array de números enteros
+	 * 
+	 * @param array
+	 * @version 1.0
+	 * @author Aarón
+	 * @return devuelve el valor mínimo
+	 **/
+	public static int minimo(int[] array) {
+		int minimo = Integer.MAX_VALUE;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < minimo) {
+				minimo = array[i];
+			}
+		}
+		return minimo;
+	}
 }
